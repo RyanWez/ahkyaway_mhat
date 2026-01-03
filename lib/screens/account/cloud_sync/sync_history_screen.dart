624cf8405038946f0ab394551402aa7a84d8fc43
RyanWez <ryanwez@example.com>
2026-01-03 14:58:54 +0630

    chore: remove unused intl import from sync history screen.

--- lib/screens/account/cloud_sync/sync_history_screen.dart
+++ lib/screens/account/cloud_sync/sync_history_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:intl/intl.dart';
 import '../../../services/sync_log_service.dart';
 import '../../../providers/theme_provider.dart';
 import '../../../theme/app_theme.dart';
